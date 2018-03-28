3d69a897423eef123ed0436d57ae7132bb39a0da
aseduto <aseduto@hotmail.com>
2018-03-28 13:40:40 +0200

    initialize

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace depsman
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int n = args.Length;
+
+            Func<int> lambda = (() =>
+            {
+                Console.WriteLine("LAMDA!");
+                return 0;
+            });
+
+            try
+            {                
+                Console.WriteLine("TEST");
+                int r = 0;
+
+                if (0 < n)
+                {
+                    r = lambda();
+                }
+
+                return r;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+                return 99;
+            }
+        }
+
+    }
+}
